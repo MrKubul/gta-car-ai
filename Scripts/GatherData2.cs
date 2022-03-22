25f816e8936af56477edfa211b9d7f235675111b
MrKubul <jakub.dryka1@gmail.com>
2022-03-22 12:54:54 +0100

    update gta plugin

--- Scripts/GatherData2.cs
+++ Scripts/GatherData2.cs
@@ -13,21 +13,23 @@ public class GatherData : Script
         Tick += OnTick;
         KeyUp += OnKeyUp;
         KeyDown += OnKeyDown;
-        Interval = 45; //interval resulting in 50ms ticks, to try different and acalculate average tick
     }
 
     private void OnTick(object sender, EventArgs e)
     {
+    }
+
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
         Ped playerPed = Game.Player.Character;
         string filepath = "vehicle_data.csv";
-
-        if (playerPed.IsInVehicle())
+        if (playerPed.IsInVehicle() & e.KeyCode == Keys.F5)
         {
             Vehicle vehicle = playerPed.CurrentVehicle;
             string timestamp = DateTime.Now.ToString("hh:mm:ss.fff");
 
-            float Sa = vehicle.SteeringAngle; //float in range [-40.0,40.0] will be scaled to [0,1]
-            float Sp = vehicle.Speed; //float mph speed divided by 2.2 will be scaled to [0,1]
+            float Sa = vehicle.SteeringAngle; //float in range [-40.0,40.0]
+            float Sp = vehicle.Speed; //float mph speed divided by 2.2
             float Tr = vehicle.Throttle; //float in range [0,1]
             float Br = vehicle.BrakePower; //float in range [0,1]
 
@@ -35,7 +37,7 @@ public class GatherData : Script
             {
                 using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
                 {
-                    file.WriteLine(timestamp + " " + Sa.ToString("F3", System.Globalization.CultureInfo.InvariantCulture) + " " + Sp.ToString("F3", System.Globalization.CultureInfo.InvariantCulture) + " " + Tr.ToString("F3", System.Globalization.CultureInfo.InvariantCulture) + " " + Br.ToString("F3", System.Globalization.CultureInfo.InvariantCulture));
+                    file.WriteLine(timestamp + "," + Sa.ToString("F3", System.Globalization.CultureInfo.InvariantCulture) + "," + Sp.ToString("F3", System.Globalization.CultureInfo.InvariantCulture) + "," + Tr.ToString("F3", System.Globalization.CultureInfo.InvariantCulture) + "," + Br.ToString("F3", System.Globalization.CultureInfo.InvariantCulture));
                 }
             }
             catch (Exception ex)
@@ -43,11 +45,6 @@ public class GatherData : Script
                 throw new ApplicationException("there was exception:", ex);
             }
         }
-    }
-
-
-    private void OnKeyDown(object sender, KeyEventArgs e)
-    {
     }
 
     private void OnKeyUp(object sender, KeyEventArgs e)
